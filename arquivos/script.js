72f717c99446e35c5cf6a1390f3530dd978125bd
Francisco Albert <albertsousalima@gmail.com>
2023-10-31 00:31:47 -0300

    trabalhando em responsividade

--- arquivos/script.js
+++ arquivos/script.js
@@ -7,10 +7,6 @@ var selectimg1 = document.querySelector('#selectimg1')
 var selectimg2 = document.querySelector('#selectimg2')
 var selectimg3 = document.querySelector('#selectimg3')
 var selectimg4 = document.querySelector('#selectimg4')
-var img1 = document.querySelector('#IMG1')
-var img2 = document.querySelector('#IMG2')
-var img3 = document.querySelector('#IMG3')
-var img4 = document.querySelector('#IMG4')
 var slide = document.querySelector('#slide')
 
 //funções para scroll vertical:
@@ -61,90 +57,8 @@ specifications.addEventListener('click', ScrollSpecifications)
 step1.addEventListener('click', Step1)
 step2.addEventListener('click', Step2)
 step3.addEventListener('click', Step3)
+window.addEventListener('load', Step1)
 
-//funções para scroll do slide:
-function Img1(){
-    img1.style.scale = '1.5'
-    img1.style.transition = '0.5s'
-    img2.style.scale = '0.8'
-    img2.style.transition = '0.5s'
-    img2.style.marginLeft = '50px'
-    img3.style.scale = '0.7'
-    img3.style.marginLeft = '-80px'
-    img3.style.transition = '0.5s'
-    img4.style.scale = '0.4'
-    img4.style.marginLeft = '-100px'
-    img4.style.transition = '0.5s'
-    selectimg1.style.backgroundColor = 'aqua'
-    selectimg1.style.scale = '1.6'
-    selectimg2.style.scale = '1'
-    selectimg2.style.backgroundColor = 'transparent'
-    selectimg3.style.backgroundColor = 'transparent'
-    selectimg3.style.scale = '1'
-    selectimg4.style.backgroundColor ='transparent'
-    selectimg4.style.scale = '1'
-}
-function Img2(){
-    img2.style.scale = '1.5'
-    img2.style.transition = '0.5s'
-    img1.style.scale = '0.8'
-    img1.style.marginRight = '0px'
-    img3.style.marginLeft = '10px'
-    img3.style.scale = '0.8'
-    img4.style.scale = '0.6'
-    img4.style.marginLeft = '-50px'
-    selectimg1.style.backgroundColor = 'transparent'
-    selectimg1.style.scale = '1'
-    selectimg2.style.scale = '1.6'
-    selectimg2.style.backgroundColor = 'aqua'
-    selectimg3.style.backgroundColor = 'transparent'
-    selectimg3.style.scale = '1'
-    selectimg4.style.backgroundColor ='transparent'
-    selectimg4.style.scale = '1'
-}
-function Img3(){
-    img2.style.scale = '0.8'
-    img2.style.transition = '0.5s'
-    img2.style.marginRight = '80px'
-    img1.style.scale = '0.4'
-    img1.style.marginRight = '-50px'
-    img3.style.scale = '1.5'
-    img3.style.transition = '0.5s'
-    img1.style.marginLeft = '30px'
-    img4.style.scale = '0.8'
-    img4.style.transition = '0.5s'
-    img4.style.marginLeft = '10px'
-    selectimg1.style.backgroundColor = 'transparent'
-    selectimg1.style.scale = '1'
-    selectimg2.style.scale = '1'
-    selectimg2.style.backgroundColor = 'transparent'
-    selectimg3.style.backgroundColor = 'aqua'
-    selectimg3.style.scale = '1.6'
-    selectimg4.style.backgroundColor ='transparent'
-    selectimg4.style.scale = '1'
-}
-function Img4(){
-    img2.style.scale = '0.7'
-    img2.style.transition = '0.5s'
-    img2.style.marginRight = '30px'
-    img1.style.scale = '0.4'
-    img1.style.marginRight = '-50px'
-    img3.style.scale = '1'
-    img3.style.transition = '0.5s'
-    img3.style.marginLeft = '-80px'
-    img4.style.scale = '1.5'
-    img4.style.marginLeft = '-20px'
-    selectimg1.style.backgroundColor = 'transparent'
-    selectimg1.style.scale = '1'
-    selectimg2.style.scale = '1'
-    selectimg2.style.backgroundColor = 'transparent'
-    selectimg3.style.backgroundColor = 'transparent'
-    selectimg3.style.scale = '1'
-    selectimg4.style.backgroundColor ='aqua'
-    selectimg4.style.scale = '1.6'
-}
-window.addEventListener('load', Img1)
-selectimg1.addEventListener('click', Img1)
-selectimg2.addEventListener('click', Img2)
-selectimg3.addEventListener('click', Img3)
-selectimg4.addEventListener('click', Img4)
+var bttsDown = document.querySelectorAll('.bttDown')
+bttsDown[2].addEventListener('click', ScrollProduct)
+bttsDown[3].addEventListener('click', ScrollSpecifications)
